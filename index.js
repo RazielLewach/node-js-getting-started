fea661c126ee967fc1b786509c008230a3b383fe
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2022-04-30 18:23:25 +0200

    db init

--- index.js
+++ index.js
@@ -15,16 +15,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+  .get('/db', (req, res) => res.render('pages/db'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
