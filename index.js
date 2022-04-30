d8f0fd52743753506179f0fbfbf648a1fa3bdf0f
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2022-04-30 18:55:13 +0200

    justcommitdamnit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const puerto = process.env.PORT || 5000;
+const app = express();
+const server = app.listen(puerto);
+const io = require('socket.io').listen(server);
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
@@ -17,16 +21,18 @@ express()
   .get('/db', (req, res) => res.render('pages/db'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-async function onRequest(request, response) {
-	try {
-		console.log("VAMOS!");
-		const client = await pool.connect();
-		const result = await client.query('SELECT * FROM test_table');
-		const results = { 'results': (result) ? result.rows : null};
-		res.render('pages/db', results );
-		client.release();
-	} catch (err) {
-		console.error(err);
-		res.send("Error " + err);
-	}
-}
+
+io.on('connection', (socket) => {
+     socket.on('onRequest', (data) => {
+        try {
+			console.log("VAMOS!");
+			const client = await pool.connect();
+			const result = await client.query('SELECT * FROM test_table');
+			const results = { 'results': (result) ? result.rows : null};
+			console.log("Resultados",results);
+			client.release();
+		} catch (err) {
+			console.error(err);
+			res.send("Error " + err);
+		}
+    });
