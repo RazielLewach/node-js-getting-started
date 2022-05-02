8d1bfcdc9db8294c7e23a3585c34a6aaf855c321
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2022-05-02 19:15:43 +0200

    justcommitdamnit

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ io.on("connection", async (socket) => {
     socket.on("login", async (name, pass) => {
 		try {
 			console.log("Check users in server");
+			const query = "SELECT * FROM users WHERE name = "+String(name)+";";
+			console.log("Query formed",query);
 			const client = await pool.connect();
-			const users = await client.query("SELECT * FROM users WHERE name = "+String(name)+";");
+			const users = await client.query(query);
 			const results = { 'results': (users) ? users.rows : null};
 			console.log("Results in server",results);
 			//if (results != null)
