bcb3c713fc9ae8bfb67b792baa79aa924ea6ea31
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2022-05-06 23:44:12 +0200

    justcommitdamnit

--- index.js
+++ index.js
@@ -133,8 +133,8 @@ io.on("connection", async (_socket) => {
 			if (_currentTale == 01 && _currentChapter == 01) _field = "https://i.imgur.com/PLMcUQi.png";
 			
 			// La posición del player.
-			var _xPlayer = 80-20;
-			var _yPlayer = 80-50;
+			var _xPlayer = 160-20;
+			var _yPlayer = 120-50;
 			
 			// Envía los datos.
 			_socket.emit("looped",_field,_xPlayer,_yPlayer);
