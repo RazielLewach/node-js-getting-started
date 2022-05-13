1f74f92ea820373f7d1bbcd345ab0f40088a1224
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2022-05-13 16:53:18 +0200

    justcommitdamnit

--- tools/scripts.js
+++ tools/scripts.js
@@ -11,32 +11,32 @@ const scripts = {
 		return _ret;
 	},
 	
-	function dcos(_ang) =>
+	dcos: (_ang) =>
 	{
 		return Math.cos(_ang*Math.PI/180);
 	},
 	
-	function dsin(_ang) =>
+	dsin: (_ang) =>
 	{
 		return Math.sin(_ang*Math.PI/180);
 	},
 	
-	function pointDistance(_x1,_y1,_x2,_y2) =>
+	pointDistance: (_x1,_y1,_x2,_y2) =>
 	{
 		return Math.sqrt(Math.pow(_x1-_x2,2)+Math.pow(_y1-_y2,2));
 	},
 	
-	function angular(_dir) =>
+	angular: (_dir) =>
 	{
 		return (_dir%360 + 360)%360;
 	},
 	
-	function pointDirection(_x1,_y1,_x2,_y2) =>
+	pointDirection: (_x1,_y1,_x2,_y2) =>
 	{
 		return angular(Math.atan2(-(_y1-_y2),_x1-_x2)*180/Math.PI);
 	},
 	
-	function absAngleDifference(_a1,_a2) =>
+	absAngleDifference: (_a1,_a2) =>
 	{
 		var _phi = Math.abs(_a2-_a1)%360;
 		var _distance = _phi > 180 ? 360 - _phi : _phi;
