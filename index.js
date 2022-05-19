89ac7d77445100d50db4b9eaa5157050b5d5cf80
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2022-05-19 20:42:30 +0200

    justcommitdamnit

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ io.on("connection", async (_socket) => {
 						// Inicializa valores default de todas las tablas generales para la nueva cuenta. No afecta a las tablas por cada tale-chapter, eso va a parte cuando abres el capítulo por primera vez.
 						doQuery("INSERT INTO chapters(name, tale, chapter) VALUES ('"+String(_name)+"',01,01);",() => {});
 						doQuery("INSERT INTO characters(name, tale, character, gender, color) VALUES ('"+String(_name)+"',01,'"+String(_name)+"','M','0');",() => {});
+						
+						// Inicializa las tablas de información permanente para cada tale.
+						doQuery("INSERT INTO player01(name,fuerzaplayer,resistenciaplayer,precisionplayer,reflejosplayer,percepcionplayer,camuflajeplayer,inteligenciaplayer,voluntadplayer,canactplayer) VALUES ('"+String(_name)+"','30','30','30','30','30','30','30','30','TRUE');",() => {});
 					});
 				}
 				// Caso: cuenta existe.
@@ -79,15 +82,13 @@ io.on("connection", async (_socket) => {
 								doQuery("UPDATE users set taleplaying = '"+String(_tale)+"', chapterplaying = '"+String(_chapter)+"' WHERE name = '"+String(_name)+"';",() => {});
 								
 								// Limpia el entorno y todos los datos.
-								doQuery("DELETE FROM player01 where name = '"+String(_name)+"';",() => {});
 								doQuery("DELETE FROM enemies01 where name = '"+String(_name)+"';",() => {});
 								
 								// Crea el entorno según el tale y chapter.
 								// HUMANO, SANGRE Y PETRÓLEO.
 								if (_tale == "01" && _chapter == "01") // Primera batalla contra el tipo con pala por haber matado a su amigo en el hielo.
 								{
-									doQuery("INSERT INTO player01(name,xplayer,yplayer,dirplayer,spriteplayer,stunplayer) VALUES ('"+String(_name)+"','0','0','0','Still','0');",() => {});
-									doQuery("INSERT INTO enemies01(name,nameenemy,xenemy,yenemy,direnemy,spriteenemy,stunenemy) VALUES ('"+String(_name)+"','ExploradorPala','10000','0','180','Chase','10');",() => {});
+									doQuery("INSERT INTO enemies01(name,nameenemy) VALUES ('"+String(_name)+"','ExploradorPala');",() => {});
 								}
 							}
 						}
@@ -162,54 +163,23 @@ io.on("connection", async (_socket) => {
 				{
 					// Lee el estado actual del jugador. Valida que no esté a mitad de un turno para poder ejecutarse.
 					doQuery("SELECT * FROM player01 WHERE name = '"+String(_name)+"';", (selPlayer) => {
-						if (selPlayer.rows[0].stunplayer == 0)
+						console.log("selPlayer.rows[0].canact",selPlayer.rows[0].canact);
+						if (selPlayer.rows[0].canact)
 						{
 							doQuery("SELECT * FROM enemies01 WHERE name = '"+String(_name)+"';", (selEnemies) => {
-								// Asigna tu sprite y cuánto te stunearás acorde a la acción recibida.
-								var _spritePlayer = "", _stunPlayer = 0;
-								if (_event == "")
-								{
-									_spritePlayer = "Still";
-									_stunPlayer = 0;
-								}
-								else if (_event == "clickWait")
-								{
-									_spritePlayer = "Still";
-									_stunPlayer = 1;
-								}
-								else if (_event == "clickTurnLeft")
-								{
-									_spritePlayer = "Look";
-									_stunPlayer = 1;
-								}
-								else if (_event == "clickTurnRight")
-								{
-									_spritePlayer = "Look";
-									_stunPlayer = 1;
-								}
-								else if (_event == "clickMoveForwards")
-								{
-									_spritePlayer = "MoveForwards";
-									_stunPlayer = 10;
-								}
-								else if (_event == "clickMoveBackwards")
-								{
-									_spritePlayer = "MoveBackwards";
-									_stunPlayer = 20;
-								}
-								else if (_event.substr(0,11) == "clickLookAt")
-								{
-									_spritePlayer = "Look";
-									_stunPlayer = 1;
-								}
-				
 								// Crea la estructura de datos del player.
+								var _canActPlayer = selPlayer.rows[0].canactplayer;
+								if (_event == "") _canActPlayer = false;
 								var _dataPlayer = {
-									xPlayer:selPlayer.rows[0].xplayer,
-									yPlayer:selPlayer.rows[0].yplayer,
-									dirPlayer:selPlayer.rows[0].dirplayer,
-									spritePlayer:_spritePlayer,
-									stunPlayer:_stunPlayer
+									fuerzaPlayer:selPlayer.rows[0].fuerzaplayer,
+									resistenciaPlayer:selPlayer.rows[0].resistenciaplayer,
+									precisionPlayer:selPlayer.rows[0].precisionplayer,
+									reflejosPlayer:selPlayer.rows[0].reflejosplayer,
+									percepcionPlayer:selPlayer.rows[0].percepcionplayer,
+									camuflajePlayer:selPlayer.rows[0].camuflajeplayer,
+									inteligenciaPlayer:selPlayer.rows[0].inteligenciaplayer,
+									voluntadPlayer:selPlayer.rows[0].voluntadplayer,
+									canActPlayer:_canActPlayer
 								};
 								
 								// Crea la estructura de datos de los enemigos.
@@ -217,12 +187,7 @@ io.on("connection", async (_socket) => {
 								for (var i = 0; i < selEnemies.rowCount; ++i)
 								{
 									_dataEnemies.push({
-										nameEnemy:selEnemies.rows[0].nameenemy,
-										xEnemy:selEnemies.rows[0].xenemy,
-										yEnemy:selEnemies.rows[0].yenemy,
-										dirEnemy:selEnemies.rows[0].direnemy,
-										spriteEnemy:selEnemies.rows[0].spriteenemy,
-										stunEnemy:selEnemies.rows[0].stunenemy
+										nameEnemy:selEnemies.rows[0].nameenemy
 									});
 								}
 								
@@ -237,21 +202,16 @@ io.on("connection", async (_socket) => {
 		
 		function loop01(_name,_event,_dataPlayer,_dataEnemies)
 		{
-			// Ejecuta la lógica sólo si es un turno de lógica. Si el stun es 0 (cargar datos) no.
-			if (_dataPlayer.stunPlayer > 0)
+			// Ejecuta la lógica sólo si es un turno de lógica. Si el cargar datos no.
+			if (_dataPlayer.canActPlayer)
+			{
 				executePlayerStep(_event,_dataPlayer);
 			
-			// Los enemigos actúan.
-			for (var i = 0; i < _dataEnemies.length; ++i)
-				executeEnemyStep(_dataEnemies[i],_dataPlayer);
+				// Los enemigos actúan.
+				for (var i = 0; i < _dataEnemies.length; ++i)
+					executeEnemyStep(_dataEnemies[i],_dataPlayer);
 			
-			// END CYCLE. Si todavía sigue paralizado, repite el loop con los datos actualizados.
-			_dataPlayer.stunPlayer = Math.max(_dataPlayer.stunPlayer-1,0);
-			if (_dataPlayer.stunPlayer > 0) loop01(_name,_event,_dataPlayer,_dataEnemies);
-			// Si ya llegó al final, guarda datos, recupera el control y envía los datos al cliente.
-			else
-			{
-				_dataPlayer.spritePlayer = "Still";
+				// Guarda los datos.
 				loop01SaveData(_name,_dataPlayer,_dataEnemies);
 			}
 		}
@@ -259,40 +219,26 @@ io.on("connection", async (_socket) => {
 		function loop01SaveData(_name,_dataPlayer,_dataEnemies)
 		{
 			// Primero guarda el player.
-			doQuery("UPDATE player01 SET xplayer = '"+String(_dataPlayer.xPlayer)+"', yplayer = '"+String(_dataPlayer.yPlayer)+"', dirplayer = '"+String(_dataPlayer.dirPlayer)+"', spriteplayer = '"+String(_dataPlayer.spritePlayer)+"', stunplayer = '"+String(_dataPlayer.stunPlayer)+"' WHERE name = '"+String(_name)+"';", () => {
+			/*doQuery("UPDATE player01 SET xplayer = '"+String(_dataPlayer.xPlayer)+"', yplayer = '"+String(_dataPlayer.yPlayer)+"', dirplayer = '"+String(_dataPlayer.dirPlayer)+"', spriteplayer = '"+String(_dataPlayer.spritePlayer)+"', stunplayer = '"+String(_dataPlayer.stunPlayer)+"' WHERE name = '"+String(_name)+"';", () => {
 				// Luego guarda cada enemigo.
 				loop01SaveDataEnemy(_name,_dataPlayer,_dataEnemies,0);
-			});
+			});*/
 		}
 		
 		function loop01SaveDataEnemy(_name,_dataPlayer,_dataEnemies,_index)
 		{
-			var _i = _index;
+			/*var _i = _index;
 			doQuery("UPDATE enemies01 SET nameenemy = '"+String(_dataEnemies[_i].nameEnemy)+"', xenemy = '"+String(_dataEnemies[_i].xEnemy)+"', yenemy = '"+String(_dataEnemies[_i].yEnemy)+"', direnemy = '"+String(_dataEnemies[_i].dirEnemy)+"', spriteenemy = '"+String(_dataEnemies[_i].spriteEnemy)+"', stunenemy = '"+String(_dataEnemies[_i].stunEnemy)+"' WHERE name = '"+String(_name)+"';", () => {
 				_i++
 				if (_i < _dataEnemies.length) loop01SaveDataEnemy(_name,_dataPlayer,_dataEnemies,_i);
 				else _socket.emit("looped01",_dataPlayer,_dataEnemies);
-			});
+			});*/
 		}
 	//}
 	//{ ####################################################### Tale 01: ejecuta un player step. #######################################################
 		function executePlayerStep(_event,_player)
 		{
-			// El player gira.
-			if (_event == "clickTurnLeft") _player.dirPlayer = angular(_player.dirPlayer+45);
-			else if (_event == "clickTurnRight") _player.dirPlayer = angular(_player.dirPlayer-45);
-			if (_event == "clickTurnLeft" || _event == "clickTurnRight") _player.dirPlayer = getMult(_player.dirPlayer,45);
-			if (_event.substr(0,11) == "clickLookAt") _player.dirPlayer = _event.substr(11,3);
 			
-			// El player se mueve.
-			var _spd = 100 - 50*(_player.spritePlayer == "MoveBackwards"), _dir = -1;
-			if (_event == "clickMoveForwards") _dir = _player.dirPlayer;
-			else if (_event == "clickMoveBackwards") _dir = _player.dirPlayer+180;
-			if (_dir != -1)
-			{
-				_player.xPlayer = Math.round(_player.xPlayer+_spd*dcos(_dir));
-				_player.yPlayer = Math.round(_player.yPlayer-_spd*dsin(_dir));
-			}
 		}
 	//}
 	//{ ####################################################### Tale 01: ejecuta un enemy step (ExploradorPala). #######################################################
@@ -300,43 +246,7 @@ io.on("connection", async (_socket) => {
 		{
 			if (_enemy.nameEnemy == "ExploradorPala")
 			{
-				// Puede decidir.
-				if (_enemy.spriteEnemy == "Still")
-				{
-					// Decide perseguirte.
-					if (pointDistance(_enemy.xEnemy,_enemy.yEnemy,_player.xPlayer,_player.yPlayer) > 700)
-					{
-						_enemy.spriteEnemy = "Chase";
-						_enemy.stunEnemy = 10;
-					}
-					
-					// Decide atacarte con la pala.
-					else
-					{
-						_enemy.spriteEnemy = "AttackPala";
-						_enemy.stunEnemy = 40;
-					}
-				}
-				// Te persigue.
-				else if (_enemy.spriteEnemy == "Chase")
-				{
-					var _spd = 40;
-					var _dir = pointDirection(_enemy.xEnemy,_enemy.yEnemy,_player.xPlayer,_player.yPlayer);
-					_enemy.xEnemy = Math.round(_enemy.xEnemy+_spd*dcos(_dir));
-					_enemy.yEnemy = Math.round(_enemy.yEnemy-_spd*dsin(_dir));
-				}
-				// Te ataca con la pala.
-				else if (_enemy.spriteEnemy == "AttackPala")
-				{
-					
-				}
-				// Gestiona animaciones.
-				if (_enemy.spriteEnemy == "Chase" || _enemy.spriteEnemy == "AttackPala")
-				{
-					_enemy.stunEnemy = Math.max(_enemy.stunEnemy-1,0);
-					if (_enemy.stunEnemy == 0) _enemy.spriteEnemy = "Still";
-				}
-			}
+				
 		}
 	//}
 });
